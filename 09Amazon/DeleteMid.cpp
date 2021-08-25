23d5eef003259030083b0bfe50872706b57e3112
Aditi Sneh <56735634+aditisneh@users.noreply.github.com>
2021-08-25 13:48:07 +0530

    :star:

---
+++ 09Amazon/DeleteMid.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node
+{
+    int data;
+    struct Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+/* Function to get the middle of the linked list*/
+struct Node *deleteMid(struct Node *head);
+void printList(Node* node) 
+{ 
+	while (node != NULL) { 
+		cout << node->data <<" "; 
+		node = node->next; 
+	}  
+	cout<<"\n";
+} 
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+
+		int data;
+		cin>>data;
+		struct Node *head = new Node(data);
+		struct Node *tail = head;
+		for (int i = 0; i < n-1; ++i)
+		{
+			cin>>data;
+			tail->next = new Node(data);
+			tail = tail->next;
+		}
+		head = deleteMid(head);
+		printList(head); 
+	}
+	return 0; 
+} 
+
+
+// } Driver Code Ends
+
+
+/* Link list Node:
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+// Deletes middle of linked list and returns head of the modified list
+Node* deleteMid(Node* head)
+{
+    if (head == NULL)
+        return NULL;
+    if (head->next == NULL) {
+        delete head;
+        return NULL;
+    }
+    // Your Code Here
+   struct Node* fast = head;
+    struct Node* slow = head;
+        struct Node* prev;
+    while (fast!= NULL && fast->next != NULL)
+            {
+                fast = fast->next->next;
+                prev = slow;
+                slow = slow->next;
+            }
+           
+             prev->next = slow->next;
+             delete slow;
+             return head;
+}
