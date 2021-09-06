6bcf33a0e9286d57e1d98ab0daa71e41f86ca427
Aditi Sneh <56735634+aditisneh@users.noreply.github.com>
2021-09-06 11:50:36 +0530

    Add files via upload

---
+++ 01 Mathematical Fundamentals/bitwise_clearIthbit.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+void clearIthBit(int &n,int i){
+	int mask = ~(1<<i);
+	n = n&mask;
+}
+int main(){
+	int n=13;
+	int i;
+	cin>>i;
+	clearIthBit(n,i);
+	cout<<n;
+	return 0;
+}
