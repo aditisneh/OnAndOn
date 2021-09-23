7a151b32a7917f752fae36b0a64623920fd5ccdf
Aditi Sneh <56735634+aditisneh@users.noreply.github.com>
2021-09-23 09:17:50 +0530

    Update rotate_array.cpp

--- 10 Leetcode/Easy/rotate_array.cpp
+++ 10 Leetcode/Easy/rotate_array.cpp
@@ -1,16 +1,1 @@
-class Solution {
-public:
-    void rotate(vector<int>& nums, int k) {
-        // int n = arr.size();
-        // if(k>n)k=k%n;
-        // while(k--){
-        // for(int i=1;i<arr.size();i++){
-        // swap(arr[0],arr[i]);\
-        //gives TLE
-        k %= nums.size();
-        reverse(nums.begin(),nums.end());
-        reverse(nums.begin(),nums.begin()+k);
-        reverse(nums.begin()+k,nums.end());
-    
-    }
-};
+
