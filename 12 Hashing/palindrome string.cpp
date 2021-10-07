0529ccc8e8c331049a60e9925d170a575d3f69fb
Your NameMuskan <muskanagrawal.eee18@itbhu.ac.in>
2021-10-07 13:14:44 +0530

    fixed error in palindrome string code

--- 12 Hashing/palindrome string.cpp
+++ 12 Hashing/palindrome string.cpp
@@ -1,29 +1,29 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main()
 {
 	string str;
-	cin>>str;
-	map<char,int> charfreq;
-	for(int i=0;i<str.size();i++)
+	cin >> str;
+	map<char, int> charfreq;
+	for (int i = 0; i < str.size(); i++)
 	{
-		mp[str[i]]++; //checking the frequency of occurence of each character in the string
+		charfreq[str[i]]++; //checking the frequency of occurence of each character in the string
 	}
-	bool odd=false;
-	for(auto it=mp.begin();it!=mp.end();it++)
+	bool odd = false;
+	for (auto it = charfreq.begin(); it != charfreq.end(); it++)
 	{
 		//any character can appear an odd number of times exactly once. If there are multiple
 		// characters that occur an odd number of times, then palindrome is not possible.
 		// Any character that apears an even number of times will always appear in pairs and hence will not
 		//be a problem.
-		if(it->second%2==1 && odd)
+		if (it->second % 2 == 1 && odd)
 		{
-			cout<<"Not Possible"<<endl;
+			cout << "Not Possible" << endl;
 			return 0;
 		}
-		if(it->second%2)
-			odd=true;
+		if (it->second % 2)
+			odd = true;
 	}
-	cout<<"Possible"<<endl;
+	cout << "Possible" << endl;
 	return 0;
 }
